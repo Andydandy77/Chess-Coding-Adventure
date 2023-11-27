a2658c41e282073a960559472a2a00afa9cf51f8
Carter Martin <97261521+Carter-Martin@users.noreply.github.com>
2023-11-27 00:25:57 -0800

    Added ConsoleUITests.cs

---
+++ Chess-Coding-Adventure-Tests/IntegrationTests/ConsoleUITests.cs
@@ -0,0 +1,44 @@
+﻿using CodingAdventureBot;
+using Microsoft.VisualStudio.TestPlatform.CommunicationUtilities;
+using System.Numerics;
+
+namespace Chess_Coding_Adventure_Tests.IntegrationTests
+{
+    internal class ConsoleUITests
+    {
+        public EngineUCI InitEngine()
+        {
+            return new EngineUCI(new Bot());
+        }
+
+		public void LogTest()
+		{
+
+		}
+        public void ResetPlayer()
+        {
+
+        }
+        public void PositionTest()
+        {
+
+        }
+        public void GoTest()
+        {
+
+        }
+        public void StopTest()
+        {
+
+        }
+        public void QuitTest()
+        {
+
+        }
+        public void BoardTest()
+        {
+
+        }
+
+}
+}
